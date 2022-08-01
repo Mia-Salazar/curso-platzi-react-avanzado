f6f9b1cdff05675ec645ad6ae1815955517b4313
Mia-Salazar <maria.sgr@gmail.com>
2022-08-01 23:17:16 +0200

    7. Add logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react'
 import { ListCategories } from './components/ListCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { CardList } from './components/CardList'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListCategories />
     <CardList />
   </div>
