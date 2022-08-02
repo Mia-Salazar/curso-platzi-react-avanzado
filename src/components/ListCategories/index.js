5d768697afd8dbb8b10b473879839b2969587340
Mia-Salazar <maria.sgr@gmail.com>
2022-08-02 15:40:50 +0200

    8. First fetch

--- src/components/ListCategories/index.js
+++ src/components/ListCategories/index.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import data from '../../../api/db.json'
 
 export const ListCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(function () {
+    window.fetch('https://petgram-server-edsf8xpy2.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+  /* with async await
+    useEffect(() => {
+        const fetchCategories = async () => {
+        const response = await window.fetch(
+            'https://petgram-server-edsf8xpy2.now.sh/categories'
+        )
+        const data = await response.json()
+        setCategories(data)
+        }
+
+        fetchCategories()
+    }, [])
+  */
+  /* with axios
+    useEffect(() => {
+        const fetchApiServerCategories = async () => {
+        const apiServer = await axios.get(
+            'https://clongram-api-j2hwstk1d.now.sh/categories'
+        );
+        setCategories(apiServer.data);
+        };
+        fetchApiServerCategories();
+    }, []);
+  */
   return (
     <List>
       {
-        data.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
