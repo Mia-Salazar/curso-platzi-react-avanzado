738d9a3ee823e5d5a7b470ab10a2beed07ade380
Mia-Salazar <maria.sgr@gmail.com>
2022-08-03 11:57:41 +0200

    12. localStorage custom hook

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
+
 import { ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const Card = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`
-
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key)
-      return like
-    } catch (e) {
-      return false
-    }
-  })
+  const [liked, setLiked] = useLocalStorage(key, false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (e) {
-      console.error(e)
-    }
-  }
   return (
     <article>
       <a href={`/detail/${id}`}>
@@ -32,7 +18,7 @@ export const Card = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
         </ImgWrapper>
       </a>
 
-      <Button onClick={() => setLocalStorage(!liked)}>
+      <Button onClick={() => setLiked(!liked)}>
         <Icon size='32px' /> {likes} likes!
       </Button>
     </article>
